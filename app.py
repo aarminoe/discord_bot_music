44c67239de69977cb77eb72238c02c274b4e9441
aarminoe <aarminoe@gmail.com>
2023-05-11 18:00:12 -0500

    added play function

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
+async def play(ctx, url: str):
+    voice_channel = ctx.author.voice.channel
+    if not voice_channel:
+        await ctx.send("You need to be in a voice channel to use this command")
+        return
+
+    if ctx.voice_client:
+        await ctx.voice_client.move_to(voice_channel)
+    else:
+        vc = await voice_channel.connect()
